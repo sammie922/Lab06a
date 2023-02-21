e48c2f0866facc420c58f128081767166f6899a9
ping ping <aauu6935@gmail.com>
2023-02-21 12:00:16 +0000

    Create a dummy endpoint

---
+++ index.ts
@@ -0,0 +1,17 @@
+import Koa from "koa";
+import Router, {RouterContext} from "koa-router";
+import logger from "koa-logger";
+import json from "koa-json";
+const app: Koa = new Koa();
+const router: Router = new Router();
+const welcomeAPI = async (ctx: RouterContext, next: any) => {
+ ctx.body = {
+ message: "Welcome to the blog API!"
+ };
+ await next();
+}
+router.get('/api/v1', welcomeAPI);
+app.use(logger());
+app.use(json());
+app.use(router.routes());
+app.listen(10888);
