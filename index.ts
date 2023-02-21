ff8e382e13bd37e46a24ffe8d7d41c977a49f370
ping ping <aauu6935@gmail.com>
2023-02-21 12:50:23 +0000

    remove rubbish

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
 import Koa from "koa";
-import Router, { RouterContext } from "koa-router";
+// import Router, { RouterContext } from "koa-router";
 import logger from "koa-logger";
 import json from "koa-json";
 const app: Koa = new Koa();
-const router: Router = new Router();
-import {router as articles} from "./routes/articles";
+// const router: Router = new Router();
+import { router as articles } from "./routes/articles";
 
 
-const welcomeAPI = async (ctx: RouterContext, next: any) => {
-  ctx.body = {
-    message: "Welcome to the blog API!"
-  };
-  await next();
-}
+// const welcomeAPI = async (ctx: RouterContext, next: any) => {
+//   ctx.body = {
+//     message: "Welcome to the blog API!"
+//   };
+//   await next();
+// }
 
-router.get('/api/v1', welcomeAPI);
+// router.get('/api/v1', welcomeAPI);
 app.use(logger());
 app.use(json());
-app.use(router.routes());
+// app.use(router.routes());
 app.use(articles.routes());
 app.listen(10888);
